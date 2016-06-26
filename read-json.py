34bc65a051cc34a289afe3e55ee695c4134ae7bd
Arvind Kumar <Arvind Kumar>
2016-06-26 10:22:48 -0700

    HERE IS MY PROGRESS.

---
+++ read-json.py
@@ -0,0 +1,11 @@
+import json
+from pprint import pprint
+
+json_data = open('MAHABHARATA.JSON').read()
+data = json.loads(json_data)
+for i in data:
+	answers = i["Answers"]
+	position = i["Correct Answer"]
+	qstr = "Question #" + i["Q #"] + ")"
+	print qstr, i["Q"]
+	print "Correct Answer:", answers[position]
