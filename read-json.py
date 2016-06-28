4af6f99bd806649e6d6e3c0b8418bc0faabb948a
sahana-h <sahanahariharan@gmail.com>
2016-06-27 22:06:03 -0700

    Corrected syntax

--- read-json.py
+++ read-json.py
@@ -7,5 +7,5 @@ for i in data:
 	answers = i["Answers"]
 	position = i["Correct Answer"]
 	qstr = "Question #" + i["Q #"] + ")"
-	print qstr, i["Q"]
-	print "Correct Answer:", answers[position]
+	print (qstr, i["Q"])
+	print ("Correct Answer:", answers[position])
